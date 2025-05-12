f2bf5bc105b6a8632186ea67b06b7060f8920f0e
Dostonbek Zaripov <148986454+Dastanbekk@users.noreply.github.com>
2025-05-12 14:35:01 +0500

    Update index.ts

--- request/index.ts
+++ request/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import cookie from "js-cookie";
 
 export const request = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: "https://admin-crm.onrender.com",
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${cookie.get("jwt")}`,
